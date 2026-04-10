b9cea1aeb04dd4a1fe60a0782f8cea8f6284f296
Andriy Parashchuk <anrey.andreevich@gmail.com>
2026-04-11 01:10:05 +0200

    Replace any

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@
 import "./globals.css";
 import { RelayEnvironmentProvider } from "react-relay";
 import { environment } from "@/relay/environment";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }: any) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>
